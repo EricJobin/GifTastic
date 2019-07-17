94c610ed47a6f600f97e767db3f6215b45d31094
Eric Jobin <ejobin@gmail.com>
2019-07-17 01:33:13 -0400

    I don't know what's going on anymore. I need sleep

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,40 +1,90 @@
 // API Homework - Giphy
 
+
+var cats = ["lion","tiger","puma","cheetah" ];
+
+function renderButtons() { //Function to render buttons  buttons
+    $("#buttons").empty();
+    for (var i = 0; i < cats.length; i++) {
+
+        var a =`<button data-person="${cats[i]}">${cats[i]}</button>`;
+        $("#buttons").append(a);
+
+        // console.log(a)
+
+    }
+};
+
+
+
+$("#add-button").on("click", function(event) {
+    console.log("addbutton");
+    event.preventDefault();
+    var cat = $("#add-button").val().trim();
+    console.log(cat);
+    cats.push(cat);
+    renderButtons();
+});
+
+
+// $("#add-button").on("click", function(event) {
+//     console.log("addbutton");
+//     event.preventDefault();
+//     var cat = $("#add-button").val().trim();
+//     console.log(cat);
+//     cats.push(cat);
+//     renderButtons();
+// });
+
+
+
+
+
+
+
 $(document).ready(function() { // This listens for a button click and will display images from giphy
-    $("button").on("click", function() {
+    $("body").on("click", function() {
         console.log("click1");
+        console.log(event);
         
-        var person = $(this).attr("data-person");
+        var person = event.srcElement.dataset.person;
+        console.log("person: "+person)
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +person + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
-        $.ajax({
-        url: queryURL,
-        method: "GET"
-        })
-        .then(function(response) {
-            console.log(response);
+        if(person != undefined){
 
-            var results = response.data;
+            $.ajax({
+            url: queryURL,
+            method: "GET"
+            })
+            .then(function(response) {
+                console.log(response);
 
-            for (var i = 0; i < results.length; i++) {
-                var gifDiv = $("<div>");
-                var rating = results[i].rating;
-                var p = $("<p>").text("Rating: " + rating);
+                var results = response.data;
 
-                var personImage = `
-                <img src="${results[i].images.fixed_height_still.url}" 
-                data-state="still" 
-                data-still="${results[i].images.fixed_height_still.url}" 
-                data-animate="${results[i].images.fixed_height.url}" 
-                id="${person}${i}">`
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $("<div>");
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
 
-                gifDiv.prepend(p);
-                gifDiv.prepend(personImage);
+                    var personImage = `
+                    <img src="${results[i].images.fixed_height_still.url}" 
+                    data-state="still" 
+                    data-still="${results[i].images.fixed_height_still.url}" 
+                    data-animate="${results[i].images.fixed_height.url}" 
+                    id="${person}${i}">`
 
-                $("#gifs-appear-here").prepend(gifDiv);
+                    gifDiv.prepend(p);
+                    gifDiv.prepend(personImage);
 
-            }
-        });
+                    $("#gifs-appear-here").prepend(gifDiv);
+
+                }
+            });
+        }
+
+
+
     });
 
 });
@@ -90,13 +140,11 @@ $(document).ready(function(){ //This function listens for clicks on gifs and ani
             // $(`#keanu9`).attr("src", event.path[0].dataset.still); these work to turn keanu9 off/on
             // $(`#keanu9`).attr("data-state", "still");
 
-            console.log(event.path[0].id);
-            var idoff = "#"+event.path[0].id;
-            console.log(idoff);
-            $(idoff).attr("src", event.path[0].dataset.still);
+            // console.log(event.path[0].id);   // I don't know, I really don't.
+            // var idoff = "#"+event.path[0].id;
+            // console.log(idoff);
+            // $(idoff).attr("src", event.path[0].dataset.still);
             // $(`#keanu9`).attr("data-state", "still");
-
-
 
 
         }
@@ -105,3 +153,7 @@ $(document).ready(function(){ //This function listens for clicks on gifs and ani
     })
 
 });
+
+
+
+renderButtons()
