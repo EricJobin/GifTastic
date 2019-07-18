8343078c1d908223ea4bf177c9fbeb476078a3cf
Eric Jobin <ejobin@gmail.com>
2019-07-17 23:36:15 -0400

    user input can be any string and it converts it to a camel case ID

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,7 +1,7 @@
 // API Homework - Giphy
 
 
-var cats = ["lion","tiger","puma","cheetah" ];
+var cats = ["Lion","Tiger","Puma","Cheetah" ];
 
 function renderButtons() { //Function to render buttons  buttons
     $("#buttons").empty();
@@ -13,23 +13,48 @@ function renderButtons() { //Function to render buttons  buttons
     }
 };
 
+//--------- These Funcions will convert user input into Camel Case -----------------------------------
+function splitAndLowerCase(rs) {
+    var lcs=rs.toLowerCase();
+    var sS = lcs.split(" ");
+    return sS;
+}
+function toCamelCase(cc){
+    var outputString;
+    for (var x=0; x<cc.length; x++){
+        if (x == 0){
+            outputString=cc[x]
+        }
+        else{
+            var newWord = cc[x].toUpperCase().charAt(0);
+            for(var y=1; y<cc[x].length; y++){
+                newWord=newWord.concat(cc[x].charAt(y))
+            }
+            outputString=outputString.concat(newWord);
+        }
+    }
+    return outputString;
+}
+function convertString(inputString){
+    return toCamelCase(splitAndLowerCase(inputString));
+}
+//----------------------------------------------------------------------------------------------
+
+
 $(document).ready(function() { // This listens for a button click and will display images from giphy
 
     $("#add-button").on("click", function(event) { // Function to add new buttons (Not Working)
-        console.log("Go! Clicked");
-        console.log(event);
+        // console.log("Go! Clicked");
+        // console.log(event);
         event.preventDefault();
 
         var cat = $("#movie-input").val().trim();
-        console.log(cat);
+        // console.log(cat);
         cats.push(cat);
         renderButtons();
     });
     
     $(document).on("click", ".xbuttons, img", function() {
-
-        
-        
         var person = event.srcElement.dataset.person;
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +person + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
@@ -47,13 +72,13 @@ $(document).ready(function() { // This listens for a button click and will displ
                     var gifDiv = $("<div>");
                     var rating = results[i].rating;
                     var p = $("<p>").text("Rating: " + rating);
-
+                    var personID = convertString(person);
                     var personImage = `
                         <img src="${results[i].images.fixed_height_still.url}" 
                         data-state="still" 
                         data-still="${results[i].images.fixed_height_still.url}" 
                         data-animate="${results[i].images.fixed_height.url}" 
-                    id="${person}${i}">`
+                        id="${personID}${i}">`// change this
 
                     gifDiv.prepend(p); //Rating
                     gifDiv.prepend(personImage); //Image
@@ -80,20 +105,8 @@ $(document).ready(function() { // This listens for a button click and will displ
         }
         // End of this Function
 
-       
-
-
 
     }) // End of $(document).on("click", ".xbuttons, .xgif, img", function()
 
-
-
-
-
-
-
     renderButtons()
-
 });
-
-movie-input
